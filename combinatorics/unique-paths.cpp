716175fc3ee993052710279beb806f0554a93efd
Manikandan A <146187632+m-a-n-i-k-a-n-d-a-n@users.noreply.github.com>
2025-01-22 10:03:38 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

---
+++ combinatorics/unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int solve(int i,int j,int m,int n)
+    {
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        else return solve(i+1,j,m,n)+solve(i,j+1,m,n);
+    }
+    int uniquePaths(int m, int n) {
+        return solve(0,0,m,n);
+    }
+};
