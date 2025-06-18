6f3cc68fcceabbd155e17b2e84e47a126c18b2e5
Manikandan A <146187632+m-a-n-i-k-a-n-d-a-n@users.noreply.github.com>
2025-06-18 14:32:10 +0530

    LC-maximum-average-subarray-i [Runtime: 5 ms; Memory: 55.7 MB]

--- array/maximum-average-subarray-i.java
+++ array/maximum-average-subarray-i.java
@@ -1,12 +1,12 @@
 class Solution {
     public double findMaxAverage(int[] arr, int k) {
         int l=0,r=0;
-        float avg=0;
+        double avg=0;
         while(r<k) {
             avg=avg+arr[r];
             r++;
         }
-        float maxavg=avg / k;
+        double maxavg=avg / k;
         while(r<arr.length) {
             avg=avg+arr[r];
             avg=avg-arr[l];
