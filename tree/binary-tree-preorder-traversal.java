2207fec8cc2337d4b33507ae69e9155997c9feb4
Manikandan A <146187632+m-a-n-i-k-a-n-d-a-n@users.noreply.github.com>
2025-06-22 09:48:53 +0530

    LC-binary-tree-preorder-traversal [Runtime: 0 ms; Memory: 42 MB]

--- tree/binary-tree-preorder-traversal.java
+++ tree/binary-tree-preorder-traversal.java
@@ -29,7 +29,7 @@
 }*/
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root==null) return null;
+        if(root==null) return new ArrayList<>();
         List<Integer> res=new ArrayList<>();
         Stack<TreeNode> s=new Stack<>();
         s.push(root);
