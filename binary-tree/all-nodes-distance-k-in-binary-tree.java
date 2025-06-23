b31e9066ef0d3f7e0759e5f4dd906af8efb97872
Manikandan A <146187632+m-a-n-i-k-a-n-d-a-n@users.noreply.github.com>
2025-06-23 11:13:05 +0530

    LC-all-nodes-distance-k-in-binary-tree [Runtime: 13 ms; Memory: 42.9 MB]

---
+++ binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> res=new ArrayList<>();
+        markParents(root);
+        bfs(target,k,res);
+        return res;
+    }
+    Map<TreeNode,TreeNode> map=new HashMap<>();
+    private void markParents(TreeNode root) {
+        if(root==null) return;
+        if(root.left!=null) {
+            map.put(root.left,root);
+            markParents(root.left);
+        }
+        if(root.right!=null) {
+            map.put(root.right,root);
+            markParents(root.right);
+        }
+    }
+    private void bfs(TreeNode target,int k,List<Integer> res) {
+        Queue<TreeNode> q=new LinkedList<>();
+        Set<TreeNode> vis=new HashSet<>();
+        q.offer(target);
+        vis.add(target);
+        while(!q.isEmpty()) {
+            int size=q.size();
+            if(k==0) break;
+            for(int i=0;i<size;i++) {
+                TreeNode curr=q.poll();
+                if(curr.left!=null && !vis.contains(curr.left)) {
+                    q.offer(curr.left);
+                    vis.add(curr.left);
+                }
+                if(curr.right!=null && !vis.contains(curr.right)) {
+                    q.offer(curr.right);
+                    vis.add(curr.right);
+                }
+                if(map.containsKey(curr) && !vis.contains(map.get(curr))) {
+                    q.offer(map.get(curr));
+                    vis.add(map.get(curr));
+                }
+            }
+            k--;
+        }
+        while(!q.isEmpty()) {
+            res.add(q.poll().val);
+        }
+    }
+}
