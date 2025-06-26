787758554616e799838fd5f407a0744fefe60432
Manikandan A <146187632+m-a-n-i-k-a-n-d-a-n@users.noreply.github.com>
2025-06-26 12:01:28 +0530

    LC-fibonacci-number [Runtime: 0 ms; Memory: 40.4 MB]

--- dynamic-programming/fibonacci-number.java
+++ dynamic-programming/fibonacci-number.java
@@ -2,6 +2,13 @@ class Solution {
     public int fib(int n) {
         if(n<=1)
             return n;
-        return fib(n-1)+fib(n-2);
+        int l=0,r=1;
+        int sum=0;
+        for(int i=2;i<=n;i++) {
+            sum=l+r;
+            l=r;
+            r=sum;
+        }
+        return sum;
     }
 }
